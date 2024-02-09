172e7127ba3f82be9318a979f56b188bf1792c1e
Paveun <a.tresierrajansen@gmail.com>
2024-02-09 14:53:30 -0500

    initial commit

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2023-12-13 23:25:56
+set PATH $PATH /home/paveun/.local/bin
+
+set --path -x XDG_DATA_DIRS $XDG_DATA_DIRS
+starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/miniconda3/bin/conda
+    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
+alias ssh="TERM=xterm-256color $(which ssh)"
